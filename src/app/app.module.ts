a48c30c0909b0b776d047b83a62d740f477465b6
Marc Almeida <73192810+marcal279@users.noreply.github.com>
2022-10-28 12:47:53 +0530

    Initialized Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AngularMaterialImports } from '../custom-imports/angular-material';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { TaskUtilityComponent } from './components/task-utility/task-utility.component';
