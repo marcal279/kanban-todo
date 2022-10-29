a4418073856114ce72dc99c4dce38f2d50970777
Marc Almeida <73192810+marcal279@users.noreply.github.com>
2022-10-29 12:18:17 +0530

    Added firebaseConfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA-cR6R1s1mrjSQl1peQcpEzYiCJbGW_gE",
+    authDomain: "angular-kanban-todo.firebaseapp.com",
+    projectId: "angular-kanban-todo",
+    storageBucket: "angular-kanban-todo.appspot.com",
+    messagingSenderId: "275545452066",
+    appId: "1:275545452066:web:62d836fbd50932c5ffa47b",
+    measurementId: "G-NZKH9H3F4W"
+  }
 };
